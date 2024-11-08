f8ff3c787b24dafbc97740dc33fdcbd0f3c20e35
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-08 17:46:02 -0300

    api creada

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'price' => 'sometimes|required|numeric',
+        ]);
+
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response(null, 204);
+    }
+}
