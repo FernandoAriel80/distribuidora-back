f0dff3ea362c8af2a29265317506da90e03e7c5c
F.Orellana <fernando_ariel1995@hotmail.com>
2024-11-09 20:01:16 -0300

    commit para render con json

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
+ */
 Route::apiResource('products', ProductController::class);
